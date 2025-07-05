fd4fa04ae53628789ad93e4d51cba5bbd70de3ac
ziad alsabounie <z.saboni@diwink.com>
2025-07-05 08:22:04 +0300

    video

--- components/shared/app-video-player.vue
+++ components/shared/app-video-player.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="apv-w">
     <video-player
-      :sources="sources"
-      :poster="poster"
-      :controls="true"
+      id="CustomVideoPlayer"
+      class="video-player vjs-theme-forest"
+      crossorigin="anonymous"
+      playsinline
+      controls
+      :sources="videoSource.sources"
+      :poster="videoSource.poster"
+      :tracks="videoSource.tracks"
+      :height="600"
+      :volume="0.4"
+      :children="[
+        // custom Video.js children component
+        // https://videojs.com/guides/options/#children
+        'mediaLoader',
+        'posterImage',
+        'bigPlayButton',
+        'loadingSpinner',
+        'controlBar',
+        'textTrackDisplay',
+      ]"
+      :controlBar="{
+        // custom Video.js control bar component
+        // https://videojs.com/guides/options/#componentname
+        volumePanel: true,
+      }"
       @ready="onPlayerReady"
     />
   </div>
@@ -12,8 +34,11 @@
 <script lang="ts">
 import { VideoPlayer } from '@videojs-player/vue';
 import 'video.js/dist/video-js.css';
+import '@videojs/http-streaming';
 import 'videojs-hls-quality-selector';
-import type { SourceObject } from 'video.js/dist/types/tech/tech';
+// custom Video.js CSS theme
+// https://github.com/videojs/themes
+// import '@videojs/themes/dist/forest/index.css';
 
 export default {
   components: {
@@ -21,37 +46,61 @@ export default {
   },
   data() {
     return {
-      sources: [
-        {
-          src: '/videos/360p.m3u8',
-          type: 'application/x-mpegURL',
-        },
-        {
-          src: '/videos/480p.m3u8',
-          type: 'application/x-mpegURL',
-        },
-        {
-          src: '/videos/720p.m3u8',
-          type: 'application/x-mpegURL',
-        },
-        {
-          src: '/videos/1080p.m3u8',
-          type: 'application/x-mpegURL',
-        },
-      ] as SourceObject[],
-      poster: 'https://example.com/path/to/poster.jpg',
+      videoSource: {
+        poster: '/images/poster/elephants-dream.jpg',
+        sources: [
+          {
+            src: '/videos/360p.m3u8',
+            type: 'application/x-mpegURL',
+            label: '360p',
+          },
+          {
+            src: '/videos/480p.m3u8',
+            type: 'application/x-mpegURL',
+            label: '480p',
+          },
+          {
+            src: '/videos/720p.m3u8',
+            type: 'application/x-mpegURL',
+            label: '720p',
+          },
+          {
+            src: '/videos/1080p.m3u8',
+            type: 'application/x-mpegURL',
+            label: '1080p',
+          },
+        ],
+        tracks: [
+          {
+            src: '/videos/chapters.vtt',
+            kind: 'chapters',
+            srclang: 'ar',
+            label: 'الاقسام',
+          },
+        ] as Array<any>,
+      },
     };
   },
   methods: {
     onPlayerReady(el) {
       const player = el.target.player;
-      console.log('Player ready', player);
-      new player.hlsQualitySelector({ displayCurrentQuality: true });
-      player.controlBar.addChild('QualitySelector');
+      console.log(player);
+      const qualityLevels = player.qualityLevels();
+      console.log(qualityLevels);
+      // qualityLevels.addQualityLevel({
+      //   id: '1',
+      // });
+      this.videoSource.sources.forEach((source, index) => {
+        qualityLevels.addQualityLevel({
+          id: index.toString(),
+          label: source.label,
+        });
+      });
     },
   },
 };
 </script>
+
 <style lang="scss" scoped>
 .apv-w {
   display: flex;
@@ -60,7 +109,6 @@ export default {
   justify-self: center;
   border: 1px solid var(--purple-8c);
   border-radius: 15px;
-  width: fit-content;
   overflow: hidden;
   box-shadow: var(--shadow-light-1);
 
